adae04330a981e1648c86258bd295b58871b87d0
Mark <mark.valdez12@imperial.ac.uk>
2015-11-20 21:10:32 +0000

    moved files

--- demo.m
+++ demo.m
@@ -1,11 +1,14 @@
-r = load('bestRBM.mat');
+startLearning;
+load('caltech101_silhouettes_28.mat');
+load('caltech101_silhouettes_28_split1.mat')
+load('bestRBM.mat');
 
-[~,classErr,misClass] = r.classify(test_data, single(test_labels));
+[~,classErr,misClass] = bestRBM.classify(test_data, single(test_labels));
 
 
 misClass = test_data(misClass,:);
 clf; visWeights(misClass',0,[0 1]); title(sprintf('Missclassified -- Error=%1.2f %%',classErr*100));
 
 nVis = 100;
-figure; visWeights(r.W(:,1:nVis));
+figure; visWeights(bestRBM.W(:,1:nVis));
 title('Sample of RBM Features');
